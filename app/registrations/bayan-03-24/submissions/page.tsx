007dea4681e57a713d75de9d58bd282d4cd87886
zishack786 <154465922+zishack786@users.noreply.github.com>
2026-02-02 23:38:07 +0000

    fix build error

--- app/registrations/bayan-03-24/submissions/page.tsx
+++ app/registrations/bayan-03-24/submissions/page.tsx
@@ -3,16 +3,18 @@ import { prisma } from "@/prisma";
 import { useState } from "react";
 
 export default async function BayyanForm() {
-  const PASSWORD = process.env["BAYYAN_03_24_PASSWORD"]!;
+  // const PASSWORD = process.env["BAYYAN_03_24_PASSWORD"]!;
   // const data = await prisma.khatiraEventRSVP.findMany({
   //   orderBy: { name: "asc" },
   // });
 
-  if (!PASSWORD) throw new Error("Invalid Password");
+  // if (!PASSWORD) throw new Error("Invalid Password");
 
-  return (
-    <div>
-      <SubmissionListAccess data={[]} password={PASSWORD} />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <SubmissionListAccess data={[]} password={PASSWORD} />
+  //   </div>
+  // );
+
+  return <div><p>You do not have permission to access this page. Please contact an admin.</p></div>
 }
